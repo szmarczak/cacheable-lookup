4a2f5a00c4864b61b33ef6062fc4f3957e703c1e
Szymon Marczak <sz.marczak@gmail.com>
2019-01-13 05:09:56 +0100

    'lul'

---
+++ index.js
@@ -0,0 +1,138 @@
+'use strict';
+const {Resolver, V4MAPPED, ADDRCONFIG} = require('dns');
+const util = require('util');
+const os = require('os');
+const Keyv = require('keyv');
+
+class CacheableLookup {
+	constructor(options = {}) {
+		const {cacheAdapter} = options;
+		this.cache = new Keyv({
+			uri: typeof cacheAdapter === 'string' && cacheAdapter,
+			store: typeof cacheAdapter !== 'string' && cacheAdapter,
+			namespace: 'cached-lookup'
+		});
+
+		this.maxTtl = options.maxTtl === 0 ? 1 : (options.maxTtl || Infinity);
+
+		this.resolver = options.resolver || new Resolver();
+		this.resolve4 = util.promisify(this.resolver.resolve4.bind(this.resolver));
+		this.resolve6 = util.promisify(this.resolver.resolve6.bind(this.resolver));
+	}
+
+	setServers(servers) {
+		this.resolver.setServers(servers);
+	}
+
+	getServers() {
+		return this.resolver.getServers();
+	}
+
+	lookup(hostname, options, callback) {
+		if (typeof options === 'function') {
+			callback = options;
+			options = {};
+		}
+
+		this.lookupAsync(hostname, options).then(result => {
+			if (options.all) {
+				callback(null, result);
+			} else {
+				callback(null, result.address, result.family);
+			}
+		}).catch(callback);
+	}
+
+	async lookupAsync(hostname, options = {}) {
+		let cached;
+
+		if (options.family !== 4 && options.family !== 6 && options.all) {
+			const [cached4, cached6] = await Promise.all([this.lookupAsync(hostname, {all: true, family: 4}), this.lookupAsync(hostname, {all: true, family: 6})]);
+			cached = [...(cached4 || []), ...(cached6 || [])];
+		} else {
+			cached = await this.query(hostname, options.family || 4);
+		}
+
+		if (cached.length === 0 && options.family !== 4 && options.hints & V4MAPPED) {
+			cached = await this.query(hostname, 4);
+
+			for (const entry of cached) {
+				entry.address = `::ffff:${entry.address}`;
+				entry.family = 6;
+			}
+		}
+
+		if (options.hints & ADDRCONFIG) {
+			let has4 = false;
+			let has6 = false;
+
+			for (const device of os.networkInterfaces()) {
+				if (device.internal) {
+					continue;
+				}
+
+				for (const iface of device) {
+					if (iface.family === 'IPv4') {
+						has4 = true;
+					}
+
+					if (iface.family === 'IPv6') {
+						has6 = true;
+					}
+
+					if (has4 && has6) {
+						break;
+					}
+				}
+			}
+
+			cached = cached.filter(entry => entry.family === 6 ? has6 : has4);
+		}
+
+		if (cached.length === 0) {
+			return undefined;
+		}
+
+		if (options.all) {
+			return cached;
+		}
+
+		return this.getEntry(cached);
+	}
+
+	async query(hostname, family) {
+		let cached = await this.cache.get(`${hostname}:${family}`);
+		if (!cached) {
+			cached = await this.queryAndCache(hostname, family);
+		}
+
+		return cached;
+	}
+
+	async queryAndCache(hostname, family) {
+		const resolve = family === 6 ? this.resolve6 : this.resolve4;
+		const entries = await resolve(hostname, {ttl: true});
+
+		if (entries.length === 0) {
+			return [];
+		}
+
+		let ttl = 0;
+		for (const entry of entries) {
+			ttl = Math.max(ttl, entry.ttl);
+			entry.family = family;
+			delete entry.ttl;
+		}
+		ttl = Math.min(this.maxTtl, ttl) * 1000;
+
+		await this.cache.set(`${hostname}:${family}`, entries, ttl);
+
+		return entries;
+	}
+
+	getEntry(entries) {
+		return entries[Math.floor(Math.random() * entries.length)];
+	}
+}
+
+module.exports = CacheableLookup;
---
+++ test.js
@@ -0,0 +1,165 @@
+import {V4MAPPED, ADDRCONFIG} from 'dns';
+import test from 'ava';
+import CacheableLookup from '.';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+class HostnameDoesntExistError extends Error {}
+
+const resolver = {
+	resolve: (hostname, options, callback) => {
+		let data = resolver.data[hostname];
+		if (!data) {
+			callback(new HostnameDoesntExistError());
+			return;
+		}
+
+		if (options.family === 4 || options.family === 6) {
+			data = data.filter(entry => entry.family === options.family);
+		}
+
+		callback(null, JSON.parse(JSON.stringify(data)));
+	},
+	resolve4: (hostname, options, callback) => {
+		return resolver.resolve(hostname, {...options, family: 4}, callback);
+	},
+	resolve6: (hostname, options, callback) => {
+		return resolver.resolve(hostname, {...options, family: 6}, callback);
+	},
+	data: {
+		localhost: [
+			{address: '127.0.0.1', family: 4, ttl: 60},
+			{address: '::ffff:127.0.0.2', family: 6, ttl: 60}
+		],
+		temporary: [
+			{address: '127.0.0.1', family: 4, ttl: 1}
+		],
+		ttl: [
+			{address: '127.0.0.1', family: 4, ttl: 1}
+		],
+		static4: [
+			{address: '127.0.0.1', family: 4, ttl: 1}
+		]
+	}
+};
+
+test('if `options.all` is falsy, then `options.family` is 4 when not defined', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	const entry = await cacheable.lookupAsync('localhost');
+	t.deepEqual(entry, {
+		address: '127.0.0.1',
+		family: 4
+	});
+});
+
+test('options.family', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	{
+		const entry = await cacheable.lookupAsync('localhost', {family: 4});
+		t.is(entry.address, '127.0.0.1');
+		t.is(entry.family, 4);
+	}
+
+	{
+		const entry = await cacheable.lookupAsync('localhost', {family: 6});
+		t.is(entry.address, '::ffff:127.0.0.2');
+		t.is(entry.family, 6);
+	}
+});
+
+test('options.all', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	const entries = await cacheable.lookupAsync('localhost', {all: true});
+	t.deepEqual(entries, [
+		{address: '127.0.0.1', family: 4},
+		{address: '::ffff:127.0.0.2', family: 6}
+	]);
+});
+
+test('options.all mixed with options.family', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	{
+		const entries = await cacheable.lookupAsync('localhost', {all: true, family: 4});
+		t.deepEqual(entries, [
+			{address: '127.0.0.1', family: 4}
+		]);
+	}
+
+	{
+		const entries = await cacheable.lookupAsync('localhost', {all: true, family: 6});
+		t.deepEqual(entries, [
+			{address: '::ffff:127.0.0.2', family: 6}
+		]);
+	}
+});
+
+test('V4MAPPED hint', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	{
+		const entries = await cacheable.lookupAsync('static4', {family: 6});
+		t.is(entries, undefined);
+	}
+
+	{
+		const entries = await cacheable.lookupAsync('static4', {family: 6, hints: V4MAPPED});
+		t.deepEqual(entries, {address: '::ffff:127.0.0.1', family: 6});
+	}
+});
+
+test.todo('ADDRCONFIG hint');
+
+test('caching works', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	let entries = await cacheable.lookupAsync('temporary', {all: true, family: 4});
+	t.deepEqual(entries, [
+		{address: '127.0.0.1', family: 4}
+	]);
+
+	resolver.data.temporary[0].address = '127.0.0.2';
+
+	entries = await cacheable.lookupAsync('temporary', {all: true, family: 4});
+	t.deepEqual(entries, [
+		{address: '127.0.0.1', family: 4}
+	]);
+});
+
+test('respects ttl', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	let entries = await cacheable.lookupAsync('ttl', {all: true, family: 4});
+	t.deepEqual(entries, [
+		{address: '127.0.0.1', family: 4}
+	]);
+
+	resolver.data.ttl[0].address = '127.0.0.2';
+
+	await sleep(2000);
+
+	entries = await cacheable.lookupAsync('ttl', {all: true, family: 4});
+	t.deepEqual(entries, [
+		{address: '127.0.0.2', family: 4}
+	]);
+});
+
+test('returns undefined if no match', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	const entry = await cacheable.lookupAsync('static4', {family: 6});
+	t.is(entry, undefined);
+});
+
+test('passes errors', async t => {
+	const cacheable = new CacheableLookup({resolver});
+
+	await t.throwsAsync(() => cacheable.lookupAsync('undefined'), HostnameDoesntExistError);
+});
+
+test.todo('.getServers()');
+test.todo('.setServers()');
+test.todo('callback style');
