szmarczak/cacheable-lookup

commit fdf95326cd41383053c9abcd12e4dc8a72f0f53d

Fallback to dns.lookup for IPv4 & IPv6 separately (#43)
Co-authored-by: Szymon Marczak <36894700+szmarczak@users.noreply.github.com>

source/index.js
@@ -63,8 +63,20 @@ const isIterable = map => {
 	return Symbol.iterator in map;
 };
 
+const ignoreNoResultErrors = dnsPromise => {
+	return dnsPromise.catch(error => {
+		if (error.code === 'ENODATA' || error.code === 'ENOTFOUND') {
+			return [];
+		}
+
+		throw error;
+	});
+};
+
 const ttl = {ttl: true};
 const all = {all: true};
+const all4 = {all: true, family: 4};
+const all6 = {all: true, family: 6};
 
 class CacheableLookup {
 	constructor({
@@ -222,23 +234,11 @@ class CacheableLookup {
 	}
 
 	async _resolve(hostname) {
-		const wrap = async promise => {
-			try {
-				return await promise;
-			} catch (error) {
-				if (error.code === 'ENODATA' || error.code === 'ENOTFOUND') {
-					return [];
-				}
-
-				throw error;
-			}
-		};
-
 		// ANY is unsafe as it doesn't trigger new queries in the underlying server.
 		const [A, AAAA] = await Promise.all([
-			this._resolve4(hostname, ttl),
-			this._resolve6(hostname, ttl)
-		].map(promise => wrap(promise)));
+			ignoreNoResultErrors(this._resolve4(hostname, ttl)),
+			ignoreNoResultErrors(this._resolve6(hostname, ttl))
+		]);
 
 		let aTtl = 0;
 		let aaaaTtl = 0;
@@ -280,21 +280,20 @@ class CacheableLookup {
 	}
 
 	async _lookup(hostname) {
-		try {
-			const entries = await this._dnsLookup(hostname, {
-				all: true
-			});
+		const [A, AAAA] = await Promise.all([
+			// Passing {all: true} doesn't return all IPv4 and IPv6 entries.
+			// See https://github.com/szmarczak/cacheable-lookup/issues/42
+			ignoreNoResultErrors(this._dnsLookup(hostname, all4)),
+			ignoreNoResultErrors(this._dnsLookup(hostname, all6))
+		]);
 
-			return {
-				entries,
-				cacheTtl: 0
-			};
-		} catch (_) {
-			return {
-				entries: [],
-				cacheTtl: 0
-			};
-		}
+		return {
+			entries: [
+				...A,
+				...AAAA
+			],
+			cacheTtl: 0
+		};
 	}
 
 	async _set(hostname, data, cacheTtl) {

tests/test.js
@@ -788,7 +788,7 @@ test.serial('fallback works', async t => {
 	t.deepEqual(resolver.counter, {
 		6: 1,
 		4: 1,
-		lookup: 2
+		lookup: 3
 	});
 
 	await sleep(100);
@@ -1015,12 +1015,21 @@ test('slow dns.lookup', async t => {
 		resolver,
 		lookup: (hostname, options, callback) => {
 			t.is(hostname, 'osHostname');
-			t.deepEqual(options, {all: true});
+			t.is(options.all, true);
+			t.true(options.family === 4 || options.family === 6);
 
 			setTimeout(() => {
-				callback(null, [
-					{address: '127.0.0.1', family: 4}
-				]);
+				if (options.family === 4) {
+					callback(null, [
+						{address: '127.0.0.1', family: 4}
+					]);
+				}
+
+				if (options.family === 6) {
+					callback(null, [
+						{address: '::1', family: 6}
+					]);
+				}
 			}, 10);
 		}
 	});

commit ed550e5d84ff7aae6633a93a5158b79afd45d0e0

Fix a regression introduced in #43
Fixes #48

source/index.js
@@ -280,20 +280,27 @@ class CacheableLookup {
 	}
 
 	async _lookup(hostname) {
-		const [A, AAAA] = await Promise.all([
-			// Passing {all: true} doesn't return all IPv4 and IPv6 entries.
-			// See https://github.com/szmarczak/cacheable-lookup/issues/42
-			ignoreNoResultErrors(this._dnsLookup(hostname, all4)),
-			ignoreNoResultErrors(this._dnsLookup(hostname, all6))
-		]);
+		try {
+			const [A, AAAA] = await Promise.all([
+				// Passing {all: true} doesn't return all IPv4 and IPv6 entries.
+				// See https://github.com/szmarczak/cacheable-lookup/issues/42
+				ignoreNoResultErrors(this._dnsLookup(hostname, all4)),
+				ignoreNoResultErrors(this._dnsLookup(hostname, all6))
+			]);
 
-		return {
-			entries: [
-				...A,
-				...AAAA
-			],
-			cacheTtl: 0
-		};
+			return {
+				entries: [
+					...A,
+					...AAAA
+				],
+				cacheTtl: 0
+			};
+		} catch {
+			return {
+				entries: [],
+				cacheTtl: 0
+			};
+		}
 	}
 
 	async _set(hostname, data, cacheTtl) {

tests/test.js
@@ -1041,3 +1041,17 @@ test('slow dns.lookup', async t => {
 		family: 4
 	});
 });
+
+test.cb.failing('throws original lookup error if not recognized', t => {
+	const cl = new CacheableLookup({
+		lookup: (hostname, options, callback) => {
+			callback(new Error('Fake DNS error'));
+		}
+	});
+
+	cl.lookup('example.test', error => {
+		t.is(error.message, 'Fake DNS error');
+
+		t.end();
+	});
+});